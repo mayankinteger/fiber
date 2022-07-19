82784717e2abe762d62a283f8d32eb7280d78ae6
Mayank <mayank.kumar@integertel.com>
2022-07-19 17:59:50 +0530

    update

--- fiber/urls.py
+++ fiber/urls.py
@@ -21,9 +21,7 @@ from django.conf.urls.static import static
 #from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('activity.urls')),
-    path('', include('bill.urls')),
-    path('feuser/', include('feuser.urls')),
+    
 ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handle404 = "fiber.views.error_404"
